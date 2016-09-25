2f2ff9150dd7f953210d00d40d5818f3b8499e3b
Heya Na <naheya1126@gmail.com>
2016-09-25 10:52:03 -0400

    Footer layout and slideshow

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ showSlides();
 function showSlides(){
 	var i;
 	var slides = document.getElementsByClassName("mySlides");
+	var sliderButton = document.getElementsByClassName("sliderButton");
 	for(i = 0; i<slides.length; i++){
 		slides[i].style.display = "none";
 	}
@@ -11,6 +12,16 @@ function showSlides(){
 	if(slideIndex > slides.length){
 		slideIndex = 1;
 	}
+	for(i = 0; i<sliderButton.length; i++){
+		sliderButton[i].className = sliderButton[i].className.replace(" active", "");
+	}
 	slides[slideIndex-1].style.display = "block";
+	sliderButton[slideIndex-1].className += " active";
 	setTimeout(showSlides, 2000);
 }
+
+// document.getElementsByClassName("sliderButton").addEventListener("click", showCurrentSlide);
+
+// function showCurrentSlide(){
+
+// }
